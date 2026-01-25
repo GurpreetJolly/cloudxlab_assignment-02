f7c998d62e024eaedecf6c5f3a05762e6e3d96a7
Gurpreet Jolly <jolly.gurpreet@email.com>
2026-01-25 09:49:55 -0500

    Removed NumPy from IQR

--- src/exercise4_6_iqr.py
+++ src/exercise4_6_iqr.py
@@ -1,13 +1,19 @@
-import numpy as np
+
 
 def compute_iqr(data):
     if not data:
-        raise ValueError("Data list is empty")
+        print("Data list is empty")
+        return None
+    
+    # sort the data
+    data = sorted(data)
 
-    q1 = np.percentile(data, 25)
-    q3 = np.percentile(data, 75)
+    q1 = data[round((0.25) * (len(data) - 1))]
+    q3 = data[round((0.75) * (len(data) - 1))]
     print(f"Q1 (25th percentile): {q1}")
     print(f"Q3 (75th percentile): {q3}")
+    # q1 = np.percentile(data, 25)
+    # q3 = np.percentile(data, 75)
     iqr = q3 - q1
     return iqr
 
@@ -20,11 +26,14 @@ def main():
         print(f"The IQR of the given data is: {iqr}")
 
         print("\n*** Some more examples ***")
+        
         example_data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        print(f"compute_iqr({example_data}): {compute_iqr(example_data)}")
+        print(f"compute_iqr({example_data})")
+        print(f"The IQR of the given data is: {compute_iqr(example_data)}")
 
         example_data = [10, 20, 30, 40, 50, 60]
-        print(f"compute_iqr({example_data}): {compute_iqr(example_data)}")
+        print(f"\ncompute_iqr({example_data})")
+        print(f"The IQR of the given data is: {compute_iqr(example_data)}")
     except ValueError as e:
         print(e)
 
